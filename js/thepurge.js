31678b468866f5607ebb80a80183014de8be57e0
tybenz <tabenziger@gmail.com>
2013-06-25 09:39:52 -0700

    first

---
+++ js/thepurge.js
@@ -0,0 +1,63 @@
+var instagramLoaded = false,
+    twitterLoaded = false,
+    $instagram,
+    instTemplate;
+
+function loadTwitterData( response ) {
+  twitterLoaded = true;
+  twitterData = response.data;
+}
+
+function loadInstagramData( response ) {
+  var data = response.data;
+
+  for ( var i = 0, length = data.length, pic; i < length; i++ ) {
+    pic = data[i];
+
+    $instagram.append( Mustache.render( instTemplate, pic ) );
+
+  }
+  $instagram.find( '.post .caption' ).each( function() {
+    var text = $( this ).text();
+
+    text = text.replace( /(\@(\S*))/g, '<a href="http://instagram.com/$2">$1</a>' );
+
+    $( this ).html( text );
+  });
+  $instagram.masonry({
+    itemSelector: '.well',
+    gutter: 30
+  });
+}
+
+function init() {
+
+  instTemplate = $( '#instagram-post-template' ).text();
+  $instagram = $( '#instagram .posts' );
+
+  var inst = new Instafeed({
+    get: 'tagged',
+    tagName: 'thepurge209',
+    clientId: '9144b57fd3bf4eb383a4bfe95f9badf1',
+    resolution: 'low_resolution',
+    success: loadInstagramData
+  });
+
+  $('.navbar a:not([data-toggle=collapse])').click( function( evt ) {
+    evt.preventDefault();
+
+    var id = $(this).attr('href');
+    $('.section').hide();
+    $(id).show();
+    if ( id == '#instagram' && $(id).find('.posts').html() == '' ) {
+      inst.run();
+    }
+    if ( id =='#twitter' ) {
+      $('#twitter iframe' ).css( 'width', '100%' );
+    }
+  });
+}
+
+window.onload = function() {
+  init();
+}
